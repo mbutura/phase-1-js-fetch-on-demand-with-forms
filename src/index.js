716cc4cc34e7fcc09b79709d020e661ba86706d5
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-12-10 22:55:26 +0000

    completed assignmrnt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
+    document.addEventListener('submit', function(evt){
+        evt.preventDefault();
+        const requestedId = evt.target.querySelector("#searchByID").value
+        fetch(`http://localhost:3000/movies/${requestedId}`)
+        .then(res => res.json())
+        .then(data => updateSummary(data))
+        evt.target.reset();
+    })
   
+}
+
+function updateSummary(data){
+    const titleNode = document.querySelector("#movieDetails h4");
+    const summaryNode = document.querySelector("#movieDetails p");
+    titleNode.textContent = data.title;
+    summaryNode.textContent = data.summary;
 }
 
 document.addEventListener('DOMContentLoaded', init);
